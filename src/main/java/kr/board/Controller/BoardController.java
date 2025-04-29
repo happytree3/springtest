c146af807d68cf61de9b6237586e551c871c142e
yoosone93 <yoosone93@naver.com>
2025-04-29 16:59:20 +0900

    250429

---
+++ src/main/java/kr/board/Controller/BoardController.java
@@ -0,0 +1,8 @@
+package kr.board.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BoardController {
+
+}
---
+++ src/main/java/kr/board/entity/Board.java
@@ -0,0 +1,12 @@
+package kr.board.entity;
+
+public class Board {
+	private int idx; //번호
+	private String title; //제목
+	private String content;
+	private String writer;
+	private String indate; //작성일
+	private int count; // 조회수
+	
+	
+}
